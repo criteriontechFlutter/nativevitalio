65551ff58a93209cc35d032925dcf8fbe76565d0
devAtCriteriontech <criteriontech1@gmail.com>
2025-04-30 12:56:29 +0530

    "worked on vital page"

--- app/src/main/java/com/criterion/nativevitalio/UI/ui/signupfragment1/SignUpFragment1.kt
+++ app/src/main/java/com/criterion/nativevitalio/UI/ui/signupfragment1/SignUpFragment1.kt
@@ -1,19 +1,15 @@
 package com.criterion.nativevitalio.UI.ui.signupfragment1
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.criterion.nativevitalio.R
+import androidx.fragment.app.viewModels
 import com.criterion.nativevitalio.UI.ui.signupFragment.SignUpFragment1ViewModel
 
 
-class SignUpFragment1 : Fragment() {
+class SignUpFragments1 : Fragment() {
 
     companion object {
-        fun newInstance() = SignUpFragment1()
+        fun newInstance() = SignUpFragments1()
     }
 
     private val viewModel: SignUpFragment1ViewModel by viewModels()
