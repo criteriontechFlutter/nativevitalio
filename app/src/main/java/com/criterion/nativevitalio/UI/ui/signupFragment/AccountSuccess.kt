0f583da8b3662006e0dbb8ebbaf57edeafeac28b
CT-Animesh <animeshsingh.ct@gmail.com>
2025-05-19 18:33:21 +0530

    Design fluid intake page and fluide intake history page

--- app/src/main/java/com/criterion/nativevitalio/UI/ui/signupFragment/AccountSuccess.kt
+++ app/src/main/java/com/criterion/nativevitalio/UI/ui/signupFragment/AccountSuccess.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.criterion.nativevitalio.R
-import com.criterion.nativevitalio.databinding.FragmentAccountSuccessBinding
+import com.critetiontech.ctvitalio.R
+
+
+import com.critetiontech.ctvitalio.databinding.FragmentAccountSuccessBinding
 
 
 class AccountSuccess : Fragment() {
