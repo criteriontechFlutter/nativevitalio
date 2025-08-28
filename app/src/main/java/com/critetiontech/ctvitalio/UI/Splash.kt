40daca5feba1793002180a25bd74076d14515722
10058 <vishalsingh@criteriontechnologies.com>
2025-08-28 11:16:46 +0530

    "fixed bug"

--- app/src/main/java/com/critetiontech/ctvitalio/UI/Splash.kt
+++ app/src/main/java/com/critetiontech/ctvitalio/UI/Splash.kt
@@ -86,9 +86,9 @@ class Splash : AppCompatActivity() {
 
     private fun navigateNextScreen() {
         try {
-            val currentPatientUHID = PrefsManager().getPatient()?.isFirstLoginCompleted.toString()
+            val currentPatientUHID = PrefsManager().getPatient()?.patientName.toString()
 
-            if (currentPatientUHID.isNotEmpty() && currentPatientUHID == "1") {
+            if (currentPatientUHID.isNotEmpty() && currentPatientUHID != "") {
                 startActivity(Intent(this, Home::class.java))
 //                val intent = Intent(this, SignupActivity::class.java)
 //                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
