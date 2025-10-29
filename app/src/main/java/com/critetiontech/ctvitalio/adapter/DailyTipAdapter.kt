b01c56c32a2f8be21f191a168a2cb5602e4cf46c
10058 <vishalsingh@criteriontechnologies.com>
2025-10-29 10:54:22 +0530

    "fixed bugs"

---
+++ app/src/main/java/com/critetiontech/ctvitalio/adapter/DailyTipAdapter.kt
@@ -0,0 +1,42 @@
+package com.critetiontech.ctvitalio.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.critetiontech.ctvitalio.R
+import com.critetiontech.ctvitalio.databinding.PriorityActionCardBinding
+
+ class DailyTipAdapter(
+    private val tips: List<DailyTip>
+) : RecyclerView.Adapter<DailyTipAdapter.TipViewHolder  >() {
+
+    inner class TipViewHolder(val binding: PriorityActionCardBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TipViewHolder {
+        val binding = PriorityActionCardBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return TipViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TipViewHolder, position: Int) {
+        val tip = tips[position]
+        holder.binding.apply {
+            ivIcon.setImageResource(tip.iconRes)
+            tvMessage.text = tip.title
+            tvDescription.text = tip.description
+            btnStartSession.text = tip.buttonText
+        }
+    }
+
+    override fun getItemCount() = tips.size
+}
+
+data class DailyTip(
+    val iconRes: Int,
+    val title: String,
+    val description: String,
+    val buttonText: String
+)
