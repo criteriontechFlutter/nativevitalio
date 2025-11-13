dd5ece6d6e9998aa836f5af4cc555a6dc9cc61e4
AnimeshCriterion <animeshsingh.ct@gmail.com>
2025-11-13 13:28:17 +0530

    feat: Update status bar color in MoodFragment
    Changes have been made to the `MoodFragment` to dynamically set the status bar color to white with dark icons for better UI consistency.
    
    Removed a hardcoded top margin from the `cardContainer` in `fragment_mood.xml` to allow the new status bar handling to correctly position the layout.

---
+++ app/src/main/java/com/critetiontech/ctvitalio/UI/UltraHumanActivity.kt
@@ -0,0 +1,17 @@
+package com.critetiontech.ctvitalio.UI
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.critetiontech.ctvitalio.R
+
+class UltraHumanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ultra_human)
+
+    }
+}
