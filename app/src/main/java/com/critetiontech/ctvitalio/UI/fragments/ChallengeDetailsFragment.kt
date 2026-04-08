afccb6dd15e84766740dcf0698e233efc7478557
AnimeshCriterion <animeshsingh.ct@gmail.com>
2026-04-08 16:29:36 +0530

    feat: Update status bar color in MoodFragment
    Changes have been made to the `MoodFragment` to dynamically set the status bar color to white with dark icons for better UI consistency.
    
    Removed a hardcoded top margin from the `cardContainer` in `fragment_mood.xml` to allow the new status bar handling to correctly position the layout.

--- app/src/main/java/com/critetiontech/ctvitalio/UI/fragments/ChallengeDetailsFragment.kt
+++ app/src/main/java/com/critetiontech/ctvitalio/UI/fragments/ChallengeDetailsFragment.kt
@@ -41,9 +41,9 @@ class ChallengeDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this)[ChallengesViewModel::class.java]
-
-viewModel.getJoinedChallengesDetailsByEmployeeId()
         challenges = arguments?.getSerializable("challenges") as? NewChallengeModel
+        viewModel.getJoinedChallengesDetailsByEmployeeId(challenges?.clientId ?:0 )
+
         binding.title.text= challenges?.title.toString()
         binding.detailId.text= challenges?.description.toString()
         binding.rewardPoints.text= challenges?.rewardPoints.toString()
