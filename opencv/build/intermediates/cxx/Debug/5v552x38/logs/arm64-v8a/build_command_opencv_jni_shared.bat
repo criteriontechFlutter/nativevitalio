89199d9a55410a9b8959d9626331648aaef7ca66
CT-Vishal <flutterdeveloperteam@gmail.com>
2026-06-01 17:23:58 +0530

    "fixed bug"

---
+++ opencv/build/intermediates/cxx/Debug/5v552x38/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\CT-VishaS\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\CT_Project\\nativevitalio\\opencv\\.cxx\\Debug\\5v552x38\\arm64-v8a" ^
+  opencv_jni_shared
